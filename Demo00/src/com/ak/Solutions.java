07e6970d63ca3064385d08e7a7b8b55d66abf3e0
Ankit Kumar Verma <122468381+akv317184@users.noreply.github.com>
2025-02-15 18:01:18 +0530

    Demo Love

---
+++ Demo00/src/com/ak/Solutions.java
@@ -0,0 +1,11 @@
+package com.ak;
+
+public class Solutions {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Demoo love you");
+
+	}
+
+}
